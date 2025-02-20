02080cc7f7a13ad1c5edfd2eeeb7545304e3764e
Amvisha <amvisha@gmail.com>
2025-02-20 22:58:03 +0200

    Second and third

---
+++ cats.py
@@ -0,0 +1,31 @@
+def get_cats_info(path: str) -> list:
+    """
+    Reads a file with information about cats and returns a list of dictionaries.
+
+    Input:
+    :param path: string     Path to text file.
+
+    Output:
+    :return: list: List of dictionaries with information about cats.
+    """
+    cats_info = []
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                cat_data = line.strip().split(',')
+                if len(cat_data) == 3:
+                    cat_dict = {
+                        "id": cat_data[0],
+                        "name": cat_data[1],
+                        "age": cat_data[2]
+                    }
+                    cats_info.append(cat_dict)
+    except FileNotFoundError:
+        print(f"Error: File '{path}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    return cats_info
+
+# Example of using the function
+# cats_info: list = get_cats_info("cats_file.txt")
+# print(cats_info)
---
+++ color.py
@@ -0,0 +1,42 @@
+import sys
+from pathlib import Path
+from colorama import Fore, init
+
+def visualize_directory_structure(directory_path):
+    """
+    Visualizes the directory structure using colors to distinguish files and folders.
+
+    Input:
+    :param directory_path: string     Path to the directory.
+    """
+    init(autoreset=True)  # Initializing colorama
+
+    directory = Path(directory_path)
+
+    if not directory.exists():
+        print(Fore.RED + f"Error: Directory '{directory_path}' does not exist.")
+        return
+
+    if not directory.is_dir():
+        print(Fore.RED + f"Error: '{directory_path}' is not a directory.")
+        return
+
+    def _visualize(path, prefix=''):
+        #Recursive function for traversing a directory.
+        items = list(path.iterdir())
+        for index, item in enumerate(items):
+            if item.is_dir():
+                print(prefix + Fore.BLUE + item.name + '/')
+                _visualize(item, prefix + '    ')
+            else:
+                print(prefix + Fore.GREEN + item.name)
+
+    print(Fore.CYAN + directory.name + '/')
+    _visualize(directory, '    ')
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Using: python color.py <directory_path>")
+    else:
+        directory_path = sys.argv[1]
+        visualize_directory_structure(directory_path)
